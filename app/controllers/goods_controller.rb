3033d32eda51417caf31079b46b4f2587340e90c
3balljuggler <3balljuggling@gmail.com>
2020-12-03 17:48:16 +0900

    デザイン修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -3,7 +3,6 @@ class GoodsController < ApplicationController
   def search
     if params[:keyword].present?
     # if params[:keyword].present? && params[:keyword].gsub(" ","").length >= 2
-      # binding.pry
       @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
       # binding.pry
     else
